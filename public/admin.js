f357a03c976eb254f469b48222568fc34b6a75fc
Cam <farrcameron@gmail.com>
2023-09-09 13:33:47 -0700

    Update

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,26 @@
 
 // Your Code Here
+async function getBooks(){
+    let response = await fetch(' http://localhost:3001/listBooks',{
+        method: 'GET',
+        headers: null,
+        body: null,
+    })
+    let result = await response.json()
+    console.log(result)
+}
+
+async function updateBook(){
+    let response = await fetch('http://localhost:3001/updateBook', {
+    method: 'PATCH',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    }),
+});
+let updatedBook = await response.json();
+console.log(updatedBook)
+}
--- public/index.js
+++ public/index.js
@@ -1,10 +1,28 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
+    let response = await fetch('http://localhost:3001/listBooks', {
+        method: 'GET',
+        headers: null,
+        body: null,
+    });
 
+    let books = await response.json();
+    console.log(books);
     books.forEach(renderBook)
 }
+
+let response = await fetch('http://localhost:3001/updateBook', {
+    method: 'PATCH',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+        "id": 3,
+        "title": "Legends of Arathrae",
+    }),
+});
+let updatedBook = await response.json();
+console.log(updatedBook)
 
 function renderBook(book) {
     let bookContainer = document.querySelector('.book-container')
